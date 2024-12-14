3d54984dcac081b429fd490e3a512099dc913ab1
Muhammad Waqas Ali <wa5371906@gmail.com>
2024-12-14 16:26:30 +0500

    Add files via upload

---
+++ M. Waqas Ali SMIT/Traffic-Light-Simulator/class/script.js
@@ -0,0 +1,58 @@
+// function outerFunction() {
+//   let myName = "Waqas";
+//   let institute = "SMIT"
+//   console.log("Outer Function Called!");
+//   return function innerFunction() {
+//     console.log(myName);
+//     console.log("Inner Function Called!");
+//     return institute                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   ;
+//   }
+// //   return innerFunction();
+// }
+// let outerFunctionVar = outerFunction();
+// // console.log(outerFunctionVar);
+
+// console.log(outerFunctionVar());
+
+// function outer(){
+//     let counter = 1;
+//     return function inner(){
+//         counter++;
+//         return `${counter} double ${counter*2}`;
+//     }
+// }
+
+// let outerVar = outer();
+
+// console.log(outerVar());
+// console.log(outerVar());
+// console.log(outerVar());
+// console.log(outerVar());
+// console.log(outerVar());
+// console.log(outerVar());
+
+
+// function array(){
+//     return arr = [
+//         {
+//           name: "Waqas",
+//           age: 19,
+//           Department: "Computer Science"
+//         },
+//         {
+//           name: "Umar",
+//           age: 20,
+//           Department: "Mathematics"
+//         },
+//         {
+//           name: "Hadi :)",
+//           age: 100,
+//           Department: "Artificial Intelligence"
+//         }
+//       ];
+// }
+
+// const [obj1,obj2,obj3] = array();
+// console.log(obj1,obj2,obj3);
+
+
